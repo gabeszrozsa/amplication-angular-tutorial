e277732268be06d8c52574fcbd356861680a38ea
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2022-11-12 15:48:29 +0100

    feat(web): frontend auth

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { CreateTaskComponent } from './create-task/create-task.component';
+import { JWTService } from './jwt.service';
+import { AuthService } from './auth.service';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskComponent, TasksComponent, CreateTaskComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    TaskComponent,
+    TasksComponent,
+    CreateTaskComponent,
+    AuthComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JWTService, multi: true },
+    AuthService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
